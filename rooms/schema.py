b9f4064cfeb6c7bb81e44a69171bceaa77df916b
JeonHG <jeonhgfm101@gmail.com>
2020-08-15 18:08:04 +0900

    see room

--- rooms/schema.py
+++ rooms/schema.py
@@ -1,25 +1,21 @@
 import graphene
-from graphene_django import DjangoObjectType
 from rooms.models import Room
-
-
-class RoomType(DjangoObjectType):
-    class Meta:
-        model = Room
-
-
-class RoomListResponse(graphene.ObjectType):
-    arr = graphene.List(RoomType)
-    total = graphene.Int()
+from .types import RoomListResponse, RoomType
 
 
 class Query(object):
     rooms = graphene.Field(RoomListResponse, page=graphene.Int())
+    room = graphene.Field(RoomType, id=graphene.Int(required=True))
 
     def resolve_rooms(self, info, page=1):
+        if page < 1:
+            page = 1
         page_size = 5
         skipping = page_size * (page - 1)
         taking = page_size * page
         rooms = Room.objects.all()[skipping:taking]
         total = Room.objects.count()
         return RoomListResponse(arr=rooms, total=total)
+
+    def resolve_room(self, info, id):
+        return Room.objects.get(id=id)
